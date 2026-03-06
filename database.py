7d259d98081847104aa4c96b69be73fd7d9e21b7
isabella-pila <isabellapilasilva@gmail.com>
2026-03-06 08:04:35 -0300

    database

--- database.py
+++ database.py
@@ -2,8 +2,7 @@ import asyncpg
 import os
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
- 'postgresql://neondb_owner:npg_Ptk3gGhlCpB7@ep-fragrant-salad-adpreuqa-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    "DATABASE_URL"
 )
 
 _pool: asyncpg.Pool = None
